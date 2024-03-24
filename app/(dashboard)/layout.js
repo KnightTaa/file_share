d115e8417d03fa9f7b50f932b85f51986755b793
TerrysDev <152247938+TerrysDev@users.noreply.github.com>
2024-03-24 19:28:10 +0530

    Initial

---
+++ app/(dashboard)/layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <div>
+        <h2>Home</h2>
+        {children}</div>
+  )
+}
+
+export default layout
---
+++ app/_utils/Constant.jsx
@@ -0,0 +1,3 @@
+export default {
+    desc: 'Drag and drop file directly on our cloud and share it with your friends secuarely with password and send it on email.'
+}
